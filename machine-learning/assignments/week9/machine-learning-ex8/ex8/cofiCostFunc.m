ff24d0d246c61130916d2d32b42edf25b7c1f1dc
212546062 <yogesh.bhardwaj1@ge.com>
2017-06-13 11:33:42 -0400

    Completed solutions for week 9 assignment

--- machine-learning/assignments/week9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning/assignments/week9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -11,7 +11,7 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -21,7 +21,7 @@ Theta_grad = zeros(size(Theta));
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
 %               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
+%               matches our costs. After that, you should implement the
 %               gradient and use the checkCostFunction routine to check
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
@@ -29,31 +29,38 @@ Theta_grad = zeros(size(Theta));
 % Notes: X - num_movies  x num_features matrix of movie features
 %        Theta - num_users  x num_features matrix of user features
 %        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the
 %            i-th movie was rated by the j-th user
 %
 % You should set the following variables correctly:
 %
-%        X_grad - num_movies x num_features matrix, containing the 
+%        X_grad - num_movies x num_features matrix, containing the
 %                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
+%        Theta_grad - num_users x num_features matrix, containing the
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+% unregularized J
+X_Theta = X * Theta';    % nm x nu
+Diff = X_Theta - Y;    % nm x nu
+Diff_R = Diff .* R;    % nm x nu
+Diff_R_squared = Diff_R .^ 2;    % nm x nu
+J = (1 / 2) * sum(Diff_R_squared(:));    % scalar value
 
+% unregularized gradients
+X_grad = Diff_R * Theta;    % [nm x nu] * [nu x 100] = [nm x 100]
+Theta_grad = Diff_R' * X;    % [nu x nm] * [nm x 100] = [nu x 100]
 
+% regularized J
+Theta_squared = Theta .^ 2;
+Theta_reg = (lambda / 2) * sum(Theta_squared(:));
+X_squared = X .^ 2;
+X_reg = (lambda / 2) * sum(X_squared(:));
+J = J + Theta_reg + X_reg;
 
-
-
-
-
-
-
-
-
-
-
-
+% regularized gradients
+X_grad = X_grad + (lambda * X);
+Theta_grad = Theta_grad + (lambda * Theta);
 
 % =============================================================
 
